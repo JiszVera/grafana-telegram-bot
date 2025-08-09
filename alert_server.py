a91843a50a4ab578d87d559c11c07f936dc8223e
JiszVera <josegregoriocastejonv1987@gmail.com>
2025-08-09 14:38:38 -0400

    alert_server.py

--- alert_server.py
+++ alert_server.py
@@ -35,6 +35,11 @@ def alert():
     if status == "firing":
         emoji = "🔴"
         title = "ALERTA ACTIVA"
+
+    # Verificar si la alerta ya fue enviada
+        if alertname in message_store:
+            return {"status": "alerta ya enviada", "message_id": message_store[alertname]
+                    
     elif status == "resolved":
         emoji = "🟢"
         title = "ALERTA RESUELTA"
@@ -86,6 +91,7 @@ def alert():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port) #esta la plantilla de donde proviene la alerta
+
 
 
 
