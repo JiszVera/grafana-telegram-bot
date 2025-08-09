517305731ffcf0f6a5ecc5c8c22f28af8d855d01
JiszVera <josegregoriocastejonv1987@gmail.com>
2025-08-08 21:11:24 -0400

    alert_server.py

--- alert_server.py
+++ alert_server.py
@@ -86,7 +86,8 @@ def alert():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) esta la plantilla de donde proviene la alerta
+    app.run(host="0.0.0.0", port=port) #esta la plantilla de donde proviene la alerta
+
 
 
 
